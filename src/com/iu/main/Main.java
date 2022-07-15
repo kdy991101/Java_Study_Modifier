1fcec07b6553d5f515a0ce0c51619b613c8617a0
kdy991101 <kdy991101@naver.com>
2022-07-15 16:06:15 +0900

    7-15 오후

--- src/com/iu/main/Main.java
+++ src/com/iu/main/Main.java
@@ -1,7 +1,7 @@
 package com.iu.main;
 import com.iu.terran.Marine;
 import com.iu.terran.Scv;
-import com.iu.zerg.Hydra;
+import com.iu.unit.Unit;
 
 
 public class Main {
@@ -9,23 +9,17 @@ public class Main {
 	public static void main(String[] args) {
 		Marine m1 = new Marine();
 		Scv s1 = new Scv();
-		Hydra h1 = new Hydra();
+		
+		//Marine is a Unit
+		//Scv is a Unit
+		
+		Unit unit = m1;
+		System.out.println(unit.getHp());
+		System.out.println(unit.getColor());
+		System.out.println(unit.getName());
 	
-		m1.setHp(100);
-		m1.setColor("화이트");//매개변수 스티링 리턴타입은 보이드
-		m1.setName("마린");
-		m1.info();
-		m1.move();
-		
-		s1.setHp(80);
-		s1.setColor("블랙");
-		s1.setName("Scv");
-		
-		h1.setHp(150);
-		h1.setColor("핑크");
-		h1.setName("히드라");
-		
-	
+		m1 = (Marine)unit;
+		System.out.println(m1.getWeapon());
 	}
 }
 
